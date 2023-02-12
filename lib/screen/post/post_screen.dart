7e85a2346635650f66a9aa30e1948724370ed896
iseruuuuu <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>
2023-02-12 19:01:33 +0900

    add1.0: ランクとアトラクション名のPickerを作成

--- lib/screen/post/post_screen.dart
+++ lib/screen/post/post_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+import 'package:disney_app/constants/attraction.dart';
 import 'package:disney_app/model/post.dart';
 import 'package:disney_app/utils/authentication.dart';
 import 'package:disney_app/utils/firestore/posts_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_picker/picker.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({Key? key}) : super(key: key);
@@ -12,8 +15,8 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   TextEditingController controller = TextEditingController();
-  TextEditingController rankController = TextEditingController();
-  TextEditingController attractionNameController = TextEditingController();
+  int rank = 0;
+  String attractionName = '';
 
   @override
   Widget build(BuildContext context) {
@@ -27,22 +30,69 @@ class _PostScreenState extends State<PostScreen> {
           TextField(
             controller: controller,
           ),
-          TextField(
-            controller: rankController,
+          GestureDetector(
+            onTap: () {
+              Picker(
+                adapter: NumberPickerAdapter(
+                  data: [const NumberPickerColumn(begin: 0, end: 100)],
+                ),
+                hideHeader: true,
+                onConfirm: (Picker picker, List value) {
+                  setState(
+                    () {
+                      rank = value[0];
+                    },
+                  );
+                },
+              ).showDialog(context);
+            },
+            child: Container(
+              width: double.infinity,
+              height: 100,
+              color: Colors.grey,
+              child: Center(
+                child: Text(
+                  rank.toString(),
+                ),
+              ),
+            ),
           ),
-          TextField(
-            controller: attractionNameController,
+          GestureDetector(
+            onTap: () {
+              Picker(
+                adapter: PickerDataAdapter<String>(
+                  pickerData: const JsonDecoder().convert(attraction),
+                ),
+                changeToFirst: true,
+                hideHeader: false,
+                onConfirm: (Picker picker, List value) {
+                  setState(() {
+                    var text1 = picker.adapter.text.replaceAll('[', '');
+                    var result2 = text1.replaceAll(']', '');
+                    var result3 = result2.replaceAll(' ', '');
+                    List<String> result4 = result3.split(',');
+                    attractionName = result4[1];
+                  });
+                },
+              ).showModal(this.context);
+            },
+            child: Container(
+              width: double.infinity,
+              height: 100,
+              color: Colors.grey.shade200,
+              child: Center(
+                child: Text(attractionName),
+              ),
+            ),
           ),
           ElevatedButton(
             onPressed: () async {
-              if (controller.text.isNotEmpty &&
-                  rankController.text.isNotEmpty &&
-                  attractionNameController.text.isNotEmpty) {
+              if (controller.text.isNotEmpty && attractionName != '') {
                 Post newPost = Post(
                   content: controller.text,
                   postAccountId: Authentication.myAccount!.id,
-                  rank: int.parse(rankController.text),
-                  attractionName: attractionNameController.text,
+                  rank: rank,
+                  attractionName: attractionName,
                 );
                 var result = await PostFirestore.addPost(newPost);
                 if (result == true) {
