859365d009be68bfe4e45c0474a69f5991b85d0c
iseruuuuu <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>
2023-07-15 15:28:11 +0900

    fix: cellのUIを調整しました。

--- lib/component/disney_cell.dart
+++ lib/component/disney_cell.dart
@@ -46,133 +46,125 @@ class DisneyCell extends StatelessWidget {
                 ),
               ),
       ),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 60,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: GestureDetector(
+                    onTap: onTapImage,
+                    child: CircleAvatar(
+                      radius: 28,
+                      foregroundImage: NetworkImage(account.imagePath),
+                    ),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2.4,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    account.name,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, right: 10, left: 10),
+                child: Text(
+                  DateFormat('yyyy/MM/dd').format(
+                    post.createdTime!.toDate(),
+                  ),
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              const Spacer(),
+              (post.postAccountId == myAccount) || isMaster
+                  ? Padding(
+                      padding: const EdgeInsets.only(top: 10, right: 20),
+                      child: GestureDetector(
+                        onTap: () {
+                          FunctionUtils.openDialog(
+                            context: context,
+                            title: '削除確認',
+                            content: '投稿を削除してもよろしいでしょうか？\n'
+                                '復元はできなくなっております',
+                            onTap: () async {
+                              PostFirestore.deletePost(post.id, post);
+                            },
+                          );
+                        },
+                        child: const Icon(Icons.reorder),
+                      ),
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.only(top: 15, right: 20),
+                      child: GestureDetector(
+                        onTap: () {
+                          FunctionUtils.openDialog(
+                            context: context,
+                            title: 'お問い合わせ画面に遷移',
+                            content: '投稿について問い合わせるサイトに\n'
+                                '遷移します。よろしいですか？',
+                            onTap: () async {
+                              final url = Uri.parse(
+                                  'https://forms.gle/Mo71XmZtA74AKBTo9');
+                              if (!await launchUrl(url)) {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                          );
+                        },
+                        child: const Icon(Icons.reorder),
+                      ),
+                    ),
+            ],
+          ),
           Padding(
-            padding: const EdgeInsets.all(10),
-            child: GestureDetector(
-              onTap: onTapImage,
-              child: CircleAvatar(
-                radius: 30,
-                foregroundImage: NetworkImage(account.imagePath),
+            padding: const EdgeInsets.only(top: 5, left: 70),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width - 80,
+              child: Text(
+                post.attractionName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.black,
+                ),
+                overflow: TextOverflow.ellipsis,
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 80,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2.8,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Text(
-                          account.name,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 10,
-                        right: 10,
-                        left: 10,
-                      ),
-                      child: Text(
-                        DateFormat('yyyy/MM/dd').format(
-                          post.createdTime!.toDate(),
-                        ),
-                        style: const TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    const Spacer(),
-                    (post.postAccountId == myAccount) || isMaster
-                        ? Padding(
-                            padding: const EdgeInsets.only(top: 10, right: 20),
-                            child: GestureDetector(
-                              onTap: () {
-                                FunctionUtils.openDialog(
-                                  context: context,
-                                  title: '削除確認',
-                                  content: '投稿を削除してもよろしいでしょうか？\n'
-                                      '復元はできなくなっております',
-                                  onTap: () async {
-                                    PostFirestore.deletePost(post.id, post);
-                                  },
-                                );
-                              },
-                              child: const Icon(Icons.reorder),
-                            ),
-                          )
-                        : Padding(
-                            padding: const EdgeInsets.only(top: 15, right: 20),
-                            child: GestureDetector(
-                              onTap: () {
-                                //TODO ブロック機能を追加する。
-                                FunctionUtils.openDialog(
-                                  context: context,
-                                  title: 'お問い合わせ画面に遷移',
-                                  content: '投稿について問い合わせるサイトに\n'
-                                      '遷移します。よろしいですか？',
-                                  onTap: () async {
-                                    final url = Uri.parse(
-                                        'https://forms.gle/Mo71XmZtA74AKBTo9');
-                                    if (!await launchUrl(url)) {
-                                      throw 'Could not launch $url';
-                                    }
-                                  },
-                                );
-                              },
-                              child: const Icon(Icons.reorder),
-                            ),
-                          ),
-                  ],
+          Padding(
+            padding: const EdgeInsets.only(top: 15, left: 70),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width - 100,
+              child: Text(
+                post.content,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                  color: Colors.black,
                 ),
+                overflow: TextOverflow.visible,
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width - 80,
-                  child: Text(
-                    post.attractionName,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.black,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width - 100,
-                  child: Text(
-                    post.content,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 15,
-                      color: Colors.black,
-                    ),
-                    overflow: TextOverflow.visible,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ],
       ),
