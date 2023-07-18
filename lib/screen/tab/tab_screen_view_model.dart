a6b0f6178dec05d84b6fefd7a11e1a91a9c0f956
iseruuuuu <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>
2023-07-18 14:01:48 +0900

    Sort directive sections alphabeticallyを修正しました。

--- lib/screen/tab/tab_screen_view_model.dart
+++ lib/screen/tab/tab_screen_view_model.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:disney_app/screen/account/account_screen.dart';
 import 'package:disney_app/screen/tab/tab_screen_state.dart';
 import 'package:disney_app/screen/time_line/time_line_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final tabScreenViewModelProvider =
     StateNotifierProvider<TabScreenViewModel, TabScreenState>(
