c89533ddc08cebb75f6bce8ed3de079871f5a51e
iseruuuuu <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>
2023-07-20 22:16:27 +0900

    add: login_screenのLoadingの機能を追記しました。

--- lib/screen/login/login_screen_view_model.dart
+++ lib/screen/login/login_screen_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:disney_app/core/model/usecase/user_firestore_usecase.dart';
 import 'package:disney_app/utils/authentication.dart';
 import 'package:disney_app/utils/function_utils.dart';
+import 'package:disney_app/utils/loading_provider.dart';
 import 'package:disney_app/utils/navigation_utils.dart';
 import 'package:disney_app/utils/snack_bar_utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -11,18 +12,21 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 final loginScreenViewModelProvider =
     ChangeNotifierProvider.autoDispose<LoginScreenViewModel>(
   (ref) {
-    return LoginScreenViewModel();
+    return LoginScreenViewModel(ref);
   },
 );
 
 class LoginScreenViewModel extends ChangeNotifier {
-  LoginScreenViewModel() {
+  LoginScreenViewModel(this.ref) {
     fetch();
   }
 
   final storage = const FlutterSecureStorage();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final AutoDisposeChangeNotifierProviderRef ref;
+
+  Loading get loading => ref.read(loadingProvider.notifier);
 
   void fetch() {
     readFromStorage();
@@ -46,6 +50,7 @@ class LoginScreenViewModel extends ChangeNotifier {
   }
 
   Future<void> login(BuildContext context, WidgetRef ref) async {
+    loading.ChangeLoading(true);
     final result = await Authentication.signIn(
       email: emailController.text,
       pass: passwordController.text,
@@ -56,12 +61,14 @@ class LoginScreenViewModel extends ChangeNotifier {
           .getUser(result.user!.uid);
 
       if (result0 == true) {
+        loading.ChangeLoading(false);
         await store();
         await Future<void>.delayed(Duration.zero).then((_) {
           return NavigationUtils.tabScreen(context);
         });
       }
     } else {
+      loading.ChangeLoading(false);
       final errorMessage = FunctionUtils().checkLoginError(result.toString());
       await Future<void>.delayed(const Duration(seconds: 1)).then((_) {
         SnackBarUtils.snackBar(context, errorMessage);
