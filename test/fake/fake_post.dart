d64b1c60a342a63209ddadaec140a65dfecb0c4c
iseruuuuu <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>
2023-07-30 18:25:37 +0900

    add: fakeのPostのデータを作成しました。

---
+++ test/fake/fake_post.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:disney_app/core/model/post.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<Post>()])
+class FakePost {
+  Post post() {
+    return Post(
+      id: '1',
+      content: 'content',
+      postAccountId: 'post_account_id',
+      createdTime: Timestamp.now(),
+      rank: 5,
+      attractionName: 'attraction_name',
+      isSpoiler: true,
+    );
+  }
+
+  List<String> mockIds = ['1', '2', '3'];
+
+  final List<Post> _mockPosts = [
+    Post(
+      id: '1',
+      content: 'content',
+      postAccountId: 'post_account_id',
+      createdTime: Timestamp.now(),
+      rank: 5,
+      attractionName: 'attraction_name',
+      isSpoiler: true,
+    ),
+    Post(
+      id: '2',
+      content: 'content2',
+      postAccountId: 'post_account_id2',
+      createdTime: Timestamp.now(),
+      rank: 5,
+      attractionName: 'attraction_name2',
+      isSpoiler: true,
+    ),
+    Post(
+      id: '3',
+      content: 'content3',
+      postAccountId: 'post_account_id3',
+      createdTime: Timestamp.now(),
+      rank: 5,
+      attractionName: 'attraction_name3',
+      isSpoiler: true,
+    ),
+  ];
+
+  List<Post> get mockPosts => _mockPosts;
+
+  String mockAccountId = 'test';
+}
