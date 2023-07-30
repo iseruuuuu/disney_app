3840321fdc52cac3b56fb143b1fdb428b0c343bc
iseruuuuu <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>
2023-07-30 22:49:00 +0900

    add: fakeのデータを追記してみました。

---
+++ test/fake/fake_user.dart
@@ -0,0 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:disney_app/core/model/account.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<User>()])
+class FakeUser {
+  Account mockAccount() {
+    return Account(
+      name: 'test',
+      userId: 'test',
+      selfIntroduction: 'test',
+      imagePath: 'test.png',
+      createdTime: Timestamp.now(),
+      updateTime: Timestamp.now(),
+    );
+  }
+
+  List<String> mockIds = ['1', '2', '3'];
+
+  final List<Account> _mockUsers = [
+    Account(
+      name: '1',
+      userId: 'test',
+      selfIntroduction: 'test',
+      imagePath: 'test.png',
+      createdTime: Timestamp.now(),
+      updateTime: Timestamp.now(),
+    ),
+    Account(
+      name: '2',
+      userId: 'test',
+      selfIntroduction: 'test',
+      imagePath: 'test.png',
+      createdTime: Timestamp.now(),
+      updateTime: Timestamp.now(),
+    ),
+    Account(
+      name: '3',
+      userId: 'test',
+      selfIntroduction: 'test',
+      imagePath: 'test.png',
+      createdTime: Timestamp.now(),
+      updateTime: Timestamp.now(),
+    ),
+  ];
+
+  List<Account> get mockUsers => _mockUsers;
+
+  String mockAccountId = 'test';
+
+  Account account1 = Account(
+    id: '1',
+    name: 'John',
+    userId: 'john123',
+    selfIntroduction: 'Hello, I am John',
+    imagePath: '/path/to/image',
+    createdTime: Timestamp.now(),
+    updateTime: Timestamp.now(),
+  );
+
+  Account account2 = Account(
+    id: '2',
+    name: 'Jane',
+    userId: 'jane123',
+    selfIntroduction: 'Hello, I am Jane',
+    imagePath: '/path/to/image',
+    createdTime: Timestamp.now(),
+    updateTime: Timestamp.now(),
+  );
+
+  String mockImage =
+      'https://quality-start.in/wp-content/uploads/2021/07/flutter-logo-sharing.png';
+}
