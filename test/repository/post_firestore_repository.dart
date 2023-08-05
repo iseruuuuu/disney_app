c2e18aaa1adb425307691d5bb989860a3736cd5c
iseruuuuu <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>
2023-08-05 13:35:49 +0900

    add: add postを記載しました。

---
+++ test/repository/post_firestore_repository.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:disney_app/core/model/api/post_firestore_api.dart';
+import 'package:disney_app/core/model/repository/post_firestore_repository.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../fake/fake_post.dart';
+import 'post_firestore_repository.mocks.dart';
+
+@GenerateMocks([
+  PostFirestoreAPI,
+  PostFirestoreRepository,
+  DocumentSnapshot,
+  WidgetRef,
+  Reference,
+  DocumentReference,
+])
+void main() {
+  late PostFirestoreRepository postFirestoreRepository;
+  late MockPostFirestoreAPI mockPostFirestoreAPI;
+  late MockDocumentSnapshot mockDocumentSnapshot;
+  late MockWidgetRef mockWidgetRef;
+  late MockReference mockReference;
+  late MockPostFirestoreRepository mockPostFirestoreRepository;
+  late MockDocumentReference mockDocumentReference;
+
+  setUp(() {
+    mockPostFirestoreAPI = MockPostFirestoreAPI();
+    mockPostFirestoreRepository = MockPostFirestoreRepository();
+    postFirestoreRepository = PostFirestoreRepository(
+      mockPostFirestoreAPI,
+    );
+    mockDocumentSnapshot = MockDocumentSnapshot();
+    mockWidgetRef = MockWidgetRef();
+    mockReference = MockReference();
+    mockDocumentReference = MockDocumentReference();
+  });
+
+  final fakePost = FakePost().post();
+  final fakeMockIds = FakePost().mockIds;
+  final fakeMockPosts = FakePost().mockPosts;
+  final fakeMockAccountId = FakePost().mockAccountId;
+
+  group('post firestore repository', () {
+    test('stream', () {
+      final controller =
+          StreamController<QuerySnapshot<Map<String, dynamic>>>();
+      when(mockPostFirestoreAPI.streamPosts())
+          .thenAnswer((_) => controller.stream);
+      final result = postFirestoreRepository.stream();
+      expect(result, isA<Stream<QuerySnapshot<Map<String, dynamic>>>>());
+      controller.close();
+    });
+
+    test('add post', () async {
+      when(mockPostFirestoreAPI.addPost(any))
+          .thenAnswer((_) async => mockDocumentReference);
+      when(mockDocumentReference.id).thenReturn('testPostId');
+      when(mockPostFirestoreAPI.addUserPost(any, any, any))
+          .thenAnswer((_) async {});
+
+      final result = await postFirestoreRepository.addPost(fakePost);
+
+      verify(mockPostFirestoreAPI.addPost(any)).called(1);
+      verify(
+        mockPostFirestoreAPI.addUserPost(fakePost.postAccountId, 'testPostId', {
+          'post_id': 'testPostId',
+          'created_time': isA<Timestamp>(),
+        }),
+      ).called(1);
+      expect(result, true);
+    });
+
+    test('addPost throws FirebaseException', () async {
+      when(mockPostFirestoreAPI.addPost(any)).thenThrow(
+        FirebaseException(plugin: 'test', message: 'test exception'),
+      );
+
+      final result = await postFirestoreRepository.addPost(fakePost);
+
+      verifyNever(mockPostFirestoreAPI.addUserPost(any, any, any));
+      expect(result, false);
+    });
+  });
+}
