29e8b38b4ac777a9236f7d00662034dde049d32e
iseruuuuu <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>
2023-08-06 22:48:39 +0900

    add: 共通のDialogを使用しました。

--- lib/screen/detail/detail_screen_view_model.dart
+++ lib/screen/detail/detail_screen_view_model.dart
@@ -1,7 +1,7 @@
 import 'package:disney_app/core/model/post.dart';
 import 'package:disney_app/core/model/usecase/post_firestore_usecase.dart';
-import 'package:disney_app/core/theme/app_text_style.dart';
 import 'package:disney_app/l10n/l10n.dart';
+import 'package:disney_app/utils/function_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,51 +22,12 @@ class DetailScreenViewModel extends ChangeNotifier {
     WidgetRef ref,
   ) {
     final l10n = L10n.of(context)!;
-    showDialog<void>(
+    FunctionUtils.openDialog(
       context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(
-            l10n.dialog_delete_check_title,
-            style: AppTextStyle.appBold17TextStyle,
-            textAlign: TextAlign.center,
-          ),
-          content: Text(
-            l10n.dialog_contact_contents,
-            style: AppTextStyle.app15TextStyle,
-            textAlign: TextAlign.center,
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: TextButton(
-                child: Text(
-                  l10n.dialog_cancel,
-                  style: AppTextStyle.cancelTextStyle,
-                ),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  deletePosts(
-                    context,
-                    accountId,
-                    newPost,
-                    ref,
-                  );
-                },
-                child: Text(
-                  l10n.dialog_ok,
-                  style: AppTextStyle.okTextStyle,
-                ),
-              ),
-            ),
-          ],
-        );
+      title: l10n.dialog_delete_check_title,
+      content: l10n.dialog_delete_check_content,
+      onTap: () {
+        deletePosts(context, accountId, newPost, ref);
       },
     );
   }
