8170e49a3761c7282c96980a7434cd9977158b71
iseruuuuu <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>
2023-08-26 09:48:50 +0900

    fix: 改行の調整をしました。

--- lib/utils/error_handling.dart
+++ lib/utils/error_handling.dart
@@ -11,31 +11,24 @@ class ErrorHandling {
       case 'invalid-email':
         result = FirebaseAuthResultStatus.invalidEmail;
         break;
-
       case 'wrong-password':
         result = FirebaseAuthResultStatus.wrongPassword;
         break;
-
       case 'user-not-found':
         result = FirebaseAuthResultStatus.userNotFound;
         break;
-
       case 'user-disabled':
         result = FirebaseAuthResultStatus.userDisabled;
         break;
-
       case 'too-many-requests':
         result = FirebaseAuthResultStatus.tooManyRequests;
         break;
-
       case 'operation-not-allowed':
         result = FirebaseAuthResultStatus.operationNotAllowed;
         break;
-
       case 'email-already-in-use':
         result = FirebaseAuthResultStatus.emailAlreadyExists;
         break;
-
       default:
         result = FirebaseAuthResultStatus.undefined;
         break;
@@ -53,31 +46,24 @@ class ErrorHandling {
       case FirebaseAuthResultStatus.invalidEmail:
         message = l10n.error_invalid_email;
         break;
-
       case FirebaseAuthResultStatus.wrongPassword:
         message = l10n.error_wrong_password;
         break;
-
       case FirebaseAuthResultStatus.userNotFound:
         message = l10n.error_user_not_found;
         break;
-
       case FirebaseAuthResultStatus.userDisabled:
         message = l10n.error_user_disabled;
         break;
-
       case FirebaseAuthResultStatus.tooManyRequests:
         message = l10n.error_too_many_requests;
         break;
-
       case FirebaseAuthResultStatus.operationNotAllowed:
         message = l10n.error_operation_not_allowed;
         break;
-
       case FirebaseAuthResultStatus.emailAlreadyExists:
         message = l10n.error_email_already_exists;
         break;
-
       case FirebaseAuthResultStatus.undefined:
         message = l10n.error_undefined;
         break;
