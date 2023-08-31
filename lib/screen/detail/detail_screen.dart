f50bd13d903f5d88a5fe6324390de510df991f75
iseruuuuu <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>
2023-08-31 12:35:48 +0900

    fix: overflowの問題を解決しました。

--- lib/screen/detail/detail_screen.dart
+++ lib/screen/detail/detail_screen.dart
@@ -31,177 +31,181 @@ class DetailScreen extends ConsumerWidget {
     final l10n = L10n.of(context)!;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const AppAppBar(icon: '🎠'),
-          Screenshot(
-            controller: ref.watch(detailViewModelProvider).screenshotController,
-            child: ColoredBox(
-              color: Colors.white,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: onTapImage,
-                        child: Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: CircleAvatar(
-                            radius: 35,
-                            foregroundImage:
-                                CachedNetworkImageProvider(account.imagePath),
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width / 2,
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 10),
-                              child: Text(
-                                account.name,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: AppTextStyle.appBoldBlack20TextStyle,
-                              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const AppAppBar(icon: '🎠'),
+            Screenshot(
+              controller:
+                  ref.watch(detailViewModelProvider).screenshotController,
+              child: ColoredBox(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: onTapImage,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15),
+                            child: CircleAvatar(
+                              radius: 35,
+                              foregroundImage:
+                                  CachedNetworkImageProvider(account.imagePath),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 5, right: 10),
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width / 1.8,
-                              child: Text(
-                                '@${account.userId}',
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: AppTextStyle.app500Grey18TextStyle,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      account.isOfficial
-                          ? Image.asset(
-                              Assets.images.official.path,
-                              fit: BoxFit.fill,
-                              width: 45,
-                            )
-                          : (post.postAccountId == MasterAccount.masterAccount)
-                              ? Image.asset(
-                                  Assets.images.dev.path,
-                                  fit: BoxFit.fill,
-                                  width: 45,
-                                )
-                              : const SizedBox.shrink(),
-                      const SizedBox(width: 10),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
                         ),
-                        child: Row(
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            AppRating(
-                              rank: post.rank,
-                              isSelect: false,
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width / 2,
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 10),
+                                child: Text(
+                                  account.name,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: AppTextStyle.appBoldBlack20TextStyle,
+                                ),
+                              ),
                             ),
                             Padding(
-                              padding: const EdgeInsets.only(left: 20),
-                              child: Text(
-                                '${post.rank}${l10n.score}',
-                                style: AppTextStyle.appBoldBlack25TextStyle,
+                              padding: const EdgeInsets.only(top: 5, right: 10),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width / 1.8,
+                                child: Text(
+                                  '@${account.userId}',
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: AppTextStyle.app500Grey18TextStyle,
+                                ),
                               ),
                             ),
                           ],
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 10,
-                        ),
-                        child: Text(
-                          post.attractionName,
-                          style: AppTextStyle.appBoldBlue20TextStyle,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 10,
-                        ),
-                        child: Text(
-                          post.content,
-                          style: AppTextStyle.appNormalBlack20TextStyle,
-                        ),
-                      ),
-                      const Divider(color: Colors.grey),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
+                        const Spacer(),
+                        account.isOfficial
+                            ? Image.asset(
+                                Assets.images.official.path,
+                                fit: BoxFit.fill,
+                                width: 45,
+                              )
+                            : (post.postAccountId ==
+                                    MasterAccount.masterAccount)
+                                ? Image.asset(
+                                    Assets.images.dev.path,
+                                    fit: BoxFit.fill,
+                                    width: 45,
+                                  )
+                                : const SizedBox.shrink(),
+                        const SizedBox(width: 10),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
                           child: Row(
                             children: [
-                              Text(
-                                DateFormat('yyyy/MM/dd').format(
-                                  post.createdTime!.toDate(),
-                                ),
-                                style: AppTextStyle.appNormalGrey15TextStyle,
-                              ),
-                              const Spacer(),
-                              IconButton(
-                                onPressed: () => ref
-                                    .read(detailViewModelProvider)
-                                    .share(post),
-                                iconSize: 30,
-                                icon: const Icon(
-                                  CupertinoIcons.share,
-                                  color: AppColorStyle.appColor,
-                                ),
+                              AppRating(
+                                rank: post.rank,
+                                isSelect: false,
                               ),
                               Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 15),
-                                child: (post.postAccountId == myAccount)
-                                    ? GestureDetector(
-                                        onTap: () => ref
-                                            .read(detailViewModelProvider)
-                                            .openCheckDialog(
-                                              context,
-                                              post.id,
-                                              post,
-                                              ref,
-                                            ),
-                                        child: const Icon(
-                                          Icons.more_horiz,
-                                          color: Colors.black,
-                                        ),
-                                      )
-                                    : Container(),
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Text(
+                                  '${post.rank}${l10n.score}',
+                                  style: AppTextStyle.appBoldBlack25TextStyle,
+                                ),
                               ),
                             ],
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          child: Text(
+                            post.attractionName,
+                            style: AppTextStyle.appBoldBlue20TextStyle,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 10,
+                          ),
+                          child: Text(
+                            post.content,
+                            style: AppTextStyle.appNormalBlack20TextStyle,
+                          ),
+                        ),
+                        const Divider(color: Colors.grey),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Row(
+                              children: [
+                                Text(
+                                  DateFormat('yyyy/MM/dd').format(
+                                    post.createdTime!.toDate(),
+                                  ),
+                                  style: AppTextStyle.appNormalGrey15TextStyle,
+                                ),
+                                const Spacer(),
+                                IconButton(
+                                  onPressed: () => ref
+                                      .read(detailViewModelProvider)
+                                      .share(post),
+                                  iconSize: 30,
+                                  icon: const Icon(
+                                    CupertinoIcons.share,
+                                    color: AppColorStyle.appColor,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 15),
+                                  child: (post.postAccountId == myAccount)
+                                      ? GestureDetector(
+                                          onTap: () => ref
+                                              .read(detailViewModelProvider)
+                                              .openCheckDialog(
+                                                context,
+                                                post.id,
+                                                post,
+                                                ref,
+                                              ),
+                                          child: const Icon(
+                                            Icons.more_horiz,
+                                            color: Colors.black,
+                                          ),
+                                        )
+                                      : Container(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
